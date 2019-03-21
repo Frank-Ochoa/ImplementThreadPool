f36b7ea17f8404a282e38710d6fa6ec9fd39fdc2
Frank-Ochoa <frankieocho@gmail.com>
2019-03-21 14:19:10 -0700

    3/21 (Revised)
    Update with: 1) Code put into package ImplementThreadPool
                 2) Working code using Semaphores
                 3) addLock and setLock are now Mutex Locks

---
+++ src/src/ImplementThreadPool/Mutex.java
@@ -0,0 +1,39 @@
+package ImplementThreadPool;
+
+public class Mutex
+{
+
+	/**
+	 * The lock status
+	 **/
+	protected boolean inuse_ = false;
+
+	public void lock() throws InterruptedException
+	{
+		if (Thread.interrupted())
+		{
+			throw new InterruptedException();
+		}
+		synchronized (this)
+		{
+			try
+			{
+				while (inuse_)
+				{
+					wait();
+				}
+				inuse_ = true;
+			} catch (InterruptedException ex)
+			{
+				notify();
+				throw ex;
+			}
+		}
+	}
+
+	public synchronized void unlock()
+	{
+		inuse_ = false;
+		notify();
+	}
+}
